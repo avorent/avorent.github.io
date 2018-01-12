2eb768d66d2a71391edc84d1f281e3197db88998
Avorent <panlau98@gmail.com>
2018-01-11 22:19:34 -0500

    lol

--- js/main.js
+++ js/main.js
@@ -1,11 +1,58 @@
 $(document).ready(function(){
 
 //#Logo (top-left of navigation bar) events:
-  $("nav").mouseenter(function(){
+  $(".navbar").mouseenter(function(){
     $("#logo").clearQueue().finish();
-    $("#logo").animate({maxWidth: "130px"}, 128);
+    $("#logo").animate({maxWidth: "150px"}, 128);
   });
-  $("nav").mouseleave(function(){
-      $("#logo").animate({maxWidth: "40px"}, 128);
+
+  $(".navbar").mouseleave(function(){
+      $("#logo").animate({maxWidth: "56px"}, 128);
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Smooth Scrolling
+  $(".navbar a, footer a[href='#myPage'").on('click', function(event){
+
+    if (this.hash !== ""){
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').clearQueue().finish();
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
 });
